264eb5e541b73416da5007b4092091d10b63ef49
Johnny van de Laar <johnny@ezcompany.nl>
2016-08-18 16:31:45 +0200

    PROBE-66 support for API's

---
+++ probe.api.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Implements hook_probe_api_info().
+ */
+function hook_probe_api_info() {
+  return array(
+    'ideal' => array(
+      'name' => 'iDEAL',
+      'extra_info' => array( // Optional array with additional information that might be useful to know.
+        'bank' => 'ing',
+      ),
+      'version' => 3,
+      'implementing_module' => 'ideal_payments', // Module that implements this API.
+    ),
+    'gtm' => array(
+      'name' => 'Google tag manager',
+      'version' => 2,
+      'implementing_module' => 'google_tag',
+    ),
+  );
+}
