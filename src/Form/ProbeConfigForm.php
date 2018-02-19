1b067abf4db5acbbee8da96a01cfad9fd38a032c
Ide Braakman <ide@ezcompany.nl>
2018-02-19 13:59:47 +0100

    Allow entering a Probe key from the UI

--- src/Form/ProbeConfigForm.php
+++ src/Form/ProbeConfigForm.php
@@ -1,14 +1,15 @@
 <?php
-/**
- * @file
- * Contains \Drupal\probe\Form\ProbeConfigForm.
- */
 
 namespace Drupal\probe\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
+/**
+ * Class ProbeConfigForm.
+ *
+ * @package Drupal\probe\Form
+ */
 class ProbeConfigForm extends ConfigFormBase {
 
   /**
@@ -24,22 +25,19 @@ class ProbeConfigForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('probe.settings');
 
-    $form['probe_xmlrpc_ips'] = array(
+    $form['probe_xmlrpc_ips'] = [
       '#title' => $this->t('Allowed IP addresses for Probe XMLRPC calls'),
       '#type' => 'textarea',
       '#default_value' => $config->get('probe_xmlrpc_ips'),
-      '#description' => $this->t('Put each IP addres on a new line. Wildcards not allowed.'),
+      '#description' => $this->t('Enter one IP address per line. Wildcards not allowed.'),
       '#required' => TRUE,
-    );
+    ];
 
-    // @todo make it work maybe with \Settings instead of \Config.
-    $form['probe_key'] = array(
+    $form['probe_key'] = [
       '#title' => $this->t('Secret key'),
       '#type' => 'textfield',
-      '#disabled' => TRUE,
       '#default_value' => $config->get('probe_key'),
-      '#description' => $this->t('Work in Progress'),
-    );
+    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -49,9 +47,9 @@ class ProbeConfigForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('probe.settings')
-     ->set('probe_xmlrpc_ips', $form_state->getValue('probe_xmlrpc_ips'))
-     ->set('probe_key', $form_state->getValue('probe_key'))
-     ->save();
+      ->set('probe_xmlrpc_ips', $form_state->getValue('probe_xmlrpc_ips'))
+      ->set('probe_key', $form_state->getValue('probe_key'))
+      ->save();
     parent::submitForm($form, $form_state);
   }
 
@@ -59,9 +57,9 @@ class ProbeConfigForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function getEditableConfigNames() {
-    return array(
+    return [
       'probe.settings',
-    );
+    ];
   }
 
 }
