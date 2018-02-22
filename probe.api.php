47b4b03d895a17c9f0c2759fe434a330139b5f80
Ide Braakman <ide@ezcompany.nl>
2018-02-22 12:17:26 +0100

    Fix coding standards issues reported by phpcs

--- probe.api.php
+++ probe.api.php
@@ -1,23 +1,41 @@
 <?php
 
 /**
- * Expose API info to the probe module.
- * Implements hook_probe_api_info().
+ * @file
+ * Probe API documentation.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Add additional information to Probe.
+ *
+ * @return array
+ *   An associative array of additional information.
  */
 function hook_probe_api_info() {
-  return array(
-    'ideal' => array(
+  return [
+    'ideal' => [
       'name' => 'iDEAL',
-      'extra_info' => array( // Optional array with additional information that might be useful to know.
+      // Optional array with information that might be useful to know.
+      'extra_info' => [
         'bank' => 'ing',
-      ),
+      ],
       'version' => 3,
-      'implementing_module' => 'ideal_payments', // Module that implements this API.
-    ),
-    'gtm' => array(
+      // Module that implements this API.
+      'implementing_module' => 'ideal_payments',
+    ],
+    'gtm' => [
       'name' => 'Google tag manager',
       'version' => 2,
       'implementing_module' => 'google_tag',
-    ),
-  );
+    ],
+  ];
 }
+
+/**
+ * @} End of "addtogroup hooks".
+ */
